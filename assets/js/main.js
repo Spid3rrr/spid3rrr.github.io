6f4c973416c8f3258037f39baaff17301ec63b11
Spid3rrr <bra.ami20@hotmail.fr>
2021-05-06 16:56:54 +0100

    Bootstrap => CSS
    Switched from bootstrap to css

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+
+function displaymsg(){
+    console.log("fired");
+    document.getElementById("answer").innerHTML = "Thank you for the review ! I appreciate it <3 ";
+    document.getElementById("answer").setAttribute("class","answer");
+}
